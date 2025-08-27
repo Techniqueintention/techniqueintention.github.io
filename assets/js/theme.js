1b6f18bf5a759df22e8f0594c01cceb3ac9c3b22
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-08-27 23:51:50 +0200

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,17 @@
-// Gestion du switch thème
 window.addEventListener("DOMContentLoaded", () => {
   const themeToggle = document.getElementById("themeSwitch");
-  if (!themeToggle) return; // évite l'erreur quand pas de switch
 
-  // Vérifie si un thème est sauvegardé
-  const savedTheme = localStorage.getItem("tiTheme") || "light";
+  // Thème par défaut = clair
+  let savedTheme = localStorage.getItem("tiTheme") || "light";
   document.documentElement.setAttribute("data-theme", savedTheme);
 
-  // Active la position du switch
-  themeToggle.checked = savedTheme === "dark";
+  if (themeToggle) {
+    themeToggle.checked = savedTheme === "dark";
 
-  themeToggle.addEventListener("change", () => {
-    const newTheme = themeToggle.checked ? "dark" : "light";
-    document.documentElement.setAttribute("data-theme", newTheme);
-    localStorage.setItem("tiTheme", newTheme);
-  });
+    themeToggle.addEventListener("change", () => {
+      const newTheme = themeToggle.checked ? "dark" : "light";
+      document.documentElement.setAttribute("data-theme", newTheme);
+      localStorage.setItem("tiTheme", newTheme);
+    });
+  }
 });
