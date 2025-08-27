90362661b048bf1ed449e3c3810052b7b706ba00
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-08-27 23:30:31 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // Main JS centralisé – importe tous les scripts
 
 import '/assets/js/loader.js';
+import '/assets/js/theme.js';
 // plus tard : import "./theme.js";
 // plus tard : import "./viewer.js";
 // etc.
