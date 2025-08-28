1717d10e61aea504784bca829aac0fb83737aa75
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-08-28 02:23:35 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@
 
 import '/assets/js/loader.js';
 import '/assets/js/theme.js';
+
+// === 📦 Modules à effets de bord ===
+import '/assets/js/canvas.js';
 // plus tard : import "./theme.js";
 // plus tard : import "./viewer.js";
 // etc.
