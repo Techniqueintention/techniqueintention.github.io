80bbacc9aaf8690e3c817f57b881c3e1d86c11ac
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-08-30 18:11:03 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,15 @@ import '/assets/js/theme.js';
 import '/assets/js/nav-active.js';
 import '/assets/js/viewer-menu.js';   // Chargé en premier
 import '/assets/js/viewer.js';        // Chargé après (contient l'accordéon)
-import '/assets/js/mobile.js';
+// On charge mobile.js dynamiquement après l'injection du menu
 
 import { IDS } from '/assets/js/ids.js';
 import { injectPartial } from '/assets/js/partials.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  injectPartial(IDS.MENU, '/assets/partials/menu.html');
+  injectPartial(IDS.MENU, '/assets/partials/menu.html').then(() => {
+    // Charge mobile.js après l'injection du menu
+    import('/assets/js/mobile.js');
+  });
   injectPartial(IDS.FOOTER, '/assets/partials/footer.html');
 });
